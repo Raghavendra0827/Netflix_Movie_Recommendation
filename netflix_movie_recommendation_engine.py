2d47fe6b7ddd6e24116e8edaeb5df8ccced3af5a
RAGHAVENDRA KN <135142090+Raghavendra0827@users.noreply.github.com>
2024-01-25 23:17:00 +0530

    Update netflix_movie_recommendation_engine.py

--- netflix_movie_recommendation_engine.py
+++ netflix_movie_recommendation_engine.py
@@ -29,15 +29,16 @@ def Recommend(User_ID, Movies):
     st.pyplot()
 
 if __name__ == "__main__":
-    user_id_input = st.text_input("Enter User ID:")
+    user_id_input = st.number_input("Enter User ID:", 822109)
 
     if user_id_input and user_id_input.isdigit():
         user_id = int(user_id_input)
+        Recommend(user_id, Movies)
         
         # Check if the entered User ID is in the list of Movie_Id
-        if user_id in list(Movies['Movie_Id']):
-            Recommend(user_id, Movies)
-        else:
-            st.warning("User ID not found in the dataset.")
-    else:
-        st.warning("Please enter a valid User ID.")
+        # if user_id in list(Movies['Movie_Id']):
+            # Recommend(user_id, Movies)
+    #     else:
+    #         st.warning("User ID not found in the dataset.")
+    # else:
+    #     st.warning("Please enter a valid User ID.")
